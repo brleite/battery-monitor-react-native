faf1f2c6b924f0b382d946b57065d3049461080a
Bruno Mayerle Leite <brleite@gmail.com>
2020-10-16 15:35:25 -0300

    Teste da API de bateria

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Battery from 'expo-battery';
 
 export default function App() {
+
+  const [batteryLevel, setBatteryLevel] = useState(-1);
+  
+  Battery.getBatteryLevelAsync().then(r => {
+    console.log("getBatteryLevelAsync");
+    console.log(r);   
+    
+    setBatteryLevel(r);
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Battery Level Format: {(batteryLevel * 100).toFixed(0) + "%"}</Text>
+      <Text>Battery Level: {batteryLevel}</Text>
       <StatusBar style="auto" />
     </View>
   );
